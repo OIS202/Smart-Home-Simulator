321900d9edc92566cae32ce5fb73fce728d085bc
Omar Shehata <o.i.m.ab.s.a@gmail.com>
2024-03-09 19:29:47 -0500

    Add Navbar and HomePage

---
+++ client/src/HomePage.js
@@ -0,0 +1,17 @@
+// HomePage.js
+import React from 'react';
+import NavBar from './NavBar';
+import Container from '@mui/material/Container';
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <Container>
+        {/* Content goes here */}
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
