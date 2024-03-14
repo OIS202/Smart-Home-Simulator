b5be5e300729a4694243b779f43cfbae5eb613d9
Khaled Saleh <93226474+khaled-saleh1@users.noreply.github.com>
2024-03-14 15:32:20 -0400

    Change SignIn/SignUp UI

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -3,24 +3,28 @@ import {
   Paper,
   TextField,
   Button,
-  Link,
-  Box,
   Typography,
   Tab,
   Tabs,
+  Box,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import logoSrc from "../iHomeLogo.png"; // Ensure this path correctly points to your logo image
+import backgroundSrc from "../Background.jpg"; // Adjust this path as necessary
 
 const SignUp = () => {
   const [file, setFile] = useState("");
   const navigate = useNavigate();
+  const theme = useTheme(); // Use the theme from MUI
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm")); // Adjust breakpoint as necessary
 
   const handleChangeTab = (event, newValue) => {
     if (newValue === 0) {
-      navigate("/signin"); // Adjust the path as necessary
+      navigate("/signin");
     } else {
-      navigate("/signup"); // Ensures we stay on the signUp page if the signUp tab is clicked again
+      navigate("/signup");
     }
   };
 
@@ -31,102 +35,121 @@ const SignUp = () => {
   return (
     <Box
       style={{
-        backgroundColor: "#2330A5",
         minHeight: "100vh",
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
+        flexDirection: isSmallScreen ? "column" : "row",
+        alignItems: isSmallScreen ? "center" : undefined,
+        backgroundColor: "#2330A5", // Apply background color consistently
       }}
     >
       <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        marginBottom={2}
-      >
-        <img src={logoSrc} alt="iHome logo" style={{ marginRight: 10 }} />
-        <Typography variant="h5" style={{ color: "#FFFFFF" }}>
-          iHome
-        </Typography>
-      </Box>
-
-      <Paper
-        elevation={10}
         style={{
-          padding: 20,
-          width: "auto",
-          maxWidth: "500px",
-          backgroundColor: "#FFFFFF",
+          display: isSmallScreen ? "none" : "block",
+          width: "60%",
+          backgroundImage: `url(${backgroundSrc})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></Box>
+      <Box
+        style={{
+          width: isSmallScreen ? "100%" : "40%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
         }}
       >
-        <Tabs
-          value={1} // Assuming SignUp is the second tab, so its index is 1
-          onChange={handleChangeTab}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          marginBottom={2}
         >
-          <Tab label="SIGN IN" />
-          <Tab label="SIGN UP" />
-        </Tabs>
-        <form>
-          <TextField label="First Name" margin="normal" fullWidth required />
-          <TextField label="Last Name" margin="normal" fullWidth required />
-          <TextField
-            label="Email"
-            margin="normal"
-            fullWidth
-            type="email"
-            required
-          />
-          <TextField
-            label="Phone Number"
-            margin="normal"
-            fullWidth
-            type="tel"
-            required
-          />
-          <TextField
-            label="Password"
-            margin="normal"
-            fullWidth
-            type="password"
-            required
-          />
-          <Typography
-            variant="body2"
-            style={{ marginTop: 16, marginBottom: 8 }}
-          >
-            House File
+          <img src={logoSrc} alt="iHome logo" style={{ marginRight: 10 }} />
+          <Typography variant="h5" style={{ color: "#FFFFFF" }}>
+            iHome
           </Typography>
-          <Button
-            variant="outlined"
-            component="label"
-            style={{
-              display: "block",
-              marginBottom: 16,
-              backgroundColor: file ? "#eceff1" : "",
-            }}
+        </Box>
+
+        <Paper
+          elevation={10}
+          style={{
+            padding: 20,
+            width: "100%",
+            maxWidth: "400px",
+            backgroundColor: "#FFFFFF",
+          }}
+        >
+          <Tabs
+            value={1}
+            onChange={handleChangeTab}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
           >
-            {file ? file.name : "Choose File"}
-            <input
-              type="file"
-              hidden
-              onChange={handleFileChange}
-              accept=".txt"
+            <Tab label="SIGN IN" />
+            <Tab label="SIGN UP" />
+          </Tabs>
+          <form>
+            <TextField label="First Name" margin="normal" fullWidth required />
+            <TextField label="Last Name" margin="normal" fullWidth required />
+            <TextField
+              label="Email"
+              margin="normal"
+              fullWidth
+              type="email"
+              required
             />
-          </Button>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            style={{ backgroundColor: "#0A1929", color: "white", margin: 8 }}
-          >
-            Sign Up
-          </Button>
-        </form>
-      </Paper>
+            <TextField
+              label="Phone Number"
+              margin="normal"
+              fullWidth
+              type="tel"
+              required
+            />
+            <TextField
+              label="Password"
+              margin="normal"
+              fullWidth
+              type="password"
+              required
+            />
+            <Typography
+              variant="body2"
+              style={{ marginTop: 16, marginBottom: 8 }}
+            >
+              House File
+            </Typography>
+            <Button
+              variant="outlined"
+              component="label"
+              style={{
+                display: "block",
+                marginBottom: 16,
+                backgroundColor: file ? "#eceff1" : "",
+              }}
+            >
+              {file ? file.name : "Choose File"}
+              <input
+                type="file"
+                hidden
+                onChange={handleFileChange}
+                accept=".txt"
+              />
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              style={{ backgroundColor: "#0A1929", color: "white", margin: 8 }}
+            >
+              Sign Up
+            </Button>
+          </form>
+        </Paper>
+      </Box>
     </Box>
   );
 };
