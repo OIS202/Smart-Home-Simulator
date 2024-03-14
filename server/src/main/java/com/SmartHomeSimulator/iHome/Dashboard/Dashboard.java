4529f949d199cf7f1c0715a10273faf6fc32d13f
Omar Shehata <o.i.m.ab.s.a@gmail.com>
2024-03-13 22:57:19 -0400

    Add observer pattern implementation

--- server/src/main/java/com/SmartHomeSimulator/iHome/Dashboard/Dashboard.java
+++ server/src/main/java/com/SmartHomeSimulator/iHome/Dashboard/Dashboard.java
@@ -1,8 +1,13 @@
 package com.SmartHomeSimulator.iHome.Dashboard;
 
+import com.SmartHomeSimulator.iHome.Observer;
+import com.SmartHomeSimulator.iHome.Modules.SmartHomeModules;
+import com.SmartHomeSimulator.iHome.Modules.SmartHomeSimulator;
+
 public class Dashboard {
 
     private static Dashboard instance;
+    private static SmartHomeSimulator shs;
 
     private Dashboard() {
         // Initialize any resources Dashboard needs
@@ -19,5 +24,12 @@ public class Dashboard {
         return instance;
     }
 
+    public void display(Observer o){
+        System.out.println("Here is the details of the module: "+o);
+    }
+
+    public void updateSHS(SmartHomeSimulator shs){
+        System.out.println("The shs "+ shs+" has been updated");
+    }
     // Dashboard related methods
 }
