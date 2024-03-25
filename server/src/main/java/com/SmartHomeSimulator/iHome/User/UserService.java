6188e5febe28b3d4aa46e95a3748c50adae629ca
Khaled Saleh <93226474+khaled-saleh1@users.noreply.github.com>
2024-03-25 14:04:11 -0400

    Fix Merge Conflicts

--- server/src/main/java/com/SmartHomeSimulator/iHome/User/UserService.java
+++ server/src/main/java/com/SmartHomeSimulator/iHome/User/UserService.java
@@ -90,7 +90,6 @@ public class UserService {
         return userRepository.findAll();
     }
 
-
     public UserResponseDto addUser(String firstName, String lastName, String email,
             String phoneNumber, String password, ObjectId houseId, UserType userType)
             throws Exception {
@@ -98,7 +97,7 @@ public class UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists.");
         }
 
-        houseId = new ObjectId("65fcdf7132513f5cebd28837"); //hard-coded object ID until further notice 
+        houseId = new ObjectId("65fcdf7132513f5cebd28837"); // hard-coded object ID until further notice
         // Create a new User object
         User newUser = new User();
         newUser.setFirstName(firstName);
@@ -121,11 +120,6 @@ public class UserService {
         return userResponseDto;
     }
 
-    //TEKA CODE
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-    
     public List<User> getUsersByHouseId(ObjectId houseId) {
         return userRepository.findAllByHouseId(houseId);
     }
