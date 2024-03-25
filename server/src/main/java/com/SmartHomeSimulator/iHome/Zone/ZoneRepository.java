60f3ae35cefb456dd782b935072d9cde15c44f20
Khaled Saleh <93226474+khaled-saleh1@users.noreply.github.com>
2024-03-24 21:14:16 -0400

    Create new Zones

--- server/src/main/java/com/SmartHomeSimulator/iHome/Zone/ZoneRepository.java
+++ server/src/main/java/com/SmartHomeSimulator/iHome/Zone/ZoneRepository.java
@@ -1,5 +1,8 @@
 package com.SmartHomeSimulator.iHome.Zone;
 
-public class ZoneRepository {
-    
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ZoneRepository extends MongoRepository<Zone, ObjectId> {
+    // No additional methods are required at this moment
 }
