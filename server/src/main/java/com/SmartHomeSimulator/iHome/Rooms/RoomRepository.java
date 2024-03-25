8eb02bbbd8a5193798e82d7f233bd15b5206dd71
Khaled Saleh <93226474+khaled-saleh1@users.noreply.github.com>
2024-03-25 00:04:23 -0400

    Add more Get methods for room

--- server/src/main/java/com/SmartHomeSimulator/iHome/Rooms/RoomRepository.java
+++ server/src/main/java/com/SmartHomeSimulator/iHome/Rooms/RoomRepository.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RoomRepository extends MongoRepository<Room, ObjectId> {
-    List<Room> findByZoneIdIsNull();
+    List<Room> findByHouseIdAndZoneIdIsNull(ObjectId houseId);
 
     List<Room> findByZoneId(ObjectId zoneId);
 
     Optional<Room> findByName(String name);
+
+    List<Room> findByHouseId(ObjectId houseId);
 
 }
